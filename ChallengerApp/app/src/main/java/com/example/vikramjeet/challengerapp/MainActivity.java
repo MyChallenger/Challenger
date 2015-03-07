f02b9d6b8edf6378214b10c6f4df38c643b1da32
Pritesh Shah <pritesh.shah@magnet.com>
2015-03-07 13:33:45 -0800

    Display username on login

--- ChallengerApp/app/src/main/java/com/example/vikramjeet/challengerapp/MainActivity.java
+++ ChallengerApp/app/src/main/java/com/example/vikramjeet/challengerapp/MainActivity.java
@@ -4,14 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.tvUserName) TextView tvUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
 
 
@@ -35,5 +45,28 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Set up the profile page based on the current user.
+        ParseUser user = ParseUser.getCurrentUser();
+        showProfile(user);
+    }
+
+    /**
+     * Shows the profile of the given user.
+     *
+     * @param user
+     */
+    private void showProfile(ParseUser user) {
+        if (user != null) {
+//            tvUserName.setText(user.getEmail());
+            String fullName = user.getString("name");
+            if (fullName != null) {
+                tvUserName.setText(fullName);
+            }
+        }
     }
 }
