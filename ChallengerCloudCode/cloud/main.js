18f56715a1b12421f663d8088bea174e8963c2be
Pritesh Shah <pritesh.shah@magnet.com>
2015-03-07 19:33:12 -0800

    Added provider and providerId attributes to ParseUser

--- ChallengerCloudCode/cloud/main.js
+++ ChallengerCloudCode/cloud/main.js
@@ -4,3 +4,16 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.beforeSave(Parse.User, function(request, response) {
+  var authData = request.object.get("authData");
+  if(typeof authData !== 'undefined') {
+    var facebook = request.object.get("authData").facebook;
+    if(typeof facebook !== 'undefined') {
+      request.object.set("provider", "facebook");
+      request.object.set("providerId", request.object.get("authData").facebook.id);
+      // console.log(request.object.get("authData"));
+    }
+  };
+  response.success();
+});
