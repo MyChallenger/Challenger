bebf333d4b78661f3569517ad78cd595b62e2b94
Pritesh Shah <pritesh.shah@magnet.com>
2015-03-10 21:16:52 -0700

    Added tests for complete and verify APIs

--- ChallengerApp/app/src/androidTest/java/com/example/vikramjeet/challengerapp/ChallengeTest.java
+++ ChallengerApp/app/src/androidTest/java/com/example/vikramjeet/challengerapp/ChallengeTest.java
@@ -6,11 +6,15 @@ import android.graphics.drawable.Drawable;
 
 import com.example.vikramjeet.challengerapp.models.Challenge;
 import com.example.vikramjeet.challengerapp.models.ChallengeStatus;
+import com.example.vikramjeet.challengerapp.models.User;
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.io.ByteArrayOutputStream;
@@ -49,7 +53,6 @@ public class ChallengeTest extends ChallengerTestCase {
             challenge.setLocation(new ParseGeoPoint(37.793992, -122.394896));
             challenge.setPrize("Asking for $10 for beer");
             challenge.setCategory("Dare");
-//            challenge.setCompleted(false);
             try {
                 challenge.save();
             } catch (ParseException e) {
@@ -85,8 +88,14 @@ public class ChallengeTest extends ChallengerTestCase {
             challenge.setLocation(new ParseGeoPoint(37.793992, -122.394896));
             challenge.setPrize("Asking for $10 for beer");
             challenge.setCategory("Dare");
-//            challenge.setCompleted(true);
             try {
+                challenge.save();
+                // Hack since we don't have a setter
+                ParseQuery<User> query = ParseQuery.getQuery(User.class);
+                query.whereEqualTo("objectId","vUpSb4YwWd");
+                User user = query.find().get(0);
+                challenge.put("backer", user);
+                challenge.put("status", "VERIFIED");
                 challenge.save();
             } catch (ParseException e) {
                 e.printStackTrace();
@@ -106,7 +115,6 @@ public class ChallengeTest extends ChallengerTestCase {
         challenge.setLocation(new ParseGeoPoint(37.793992, -122.394896));
         challenge.setPrize("Asking for $10 for beer");
         challenge.setCategory("Dare");
-//        challenge.setCompleted(false);
         // We will save the createdMedia first and then save the challenge if the upload succeeds!
         Drawable backgroundImage = getContext().getResources().getDrawable(R.drawable.background);
         if (backgroundImage != null) {
@@ -146,7 +154,6 @@ public class ChallengeTest extends ChallengerTestCase {
         challenge.setLocation(new ParseGeoPoint(37.793992, -122.394896));
         challenge.setPrize("Asking for $10 for beer");
         challenge.setCategory("Dare");
-//        challenge.setCompleted(false);
         challenge.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
@@ -157,6 +164,91 @@ public class ChallengeTest extends ChallengerTestCase {
                     public void done(Challenge backedChallenge, ParseException e) {
                         // This challenge should be backed!
                         assertNotNull(backedChallenge.backer());
+                        assertEquals(backedChallenge.getStatus(), ChallengeStatus.BACKED);
+                        countDown();
+                    }
+                });
+            }
+        });
+        await();
+    }
+
+    public void testComplete() {
+        // Let's create a challenge first
+        final Challenge challenge = new Challenge();
+        challenge.setTitle("Backed challenge");
+        challenge.setDescription("Will record a video and post!");
+        challenge.setExpiryDate(new Date());
+        challenge.setNumberOfLikes(100);
+        challenge.setNumberOfViews(205678);
+        challenge.setNumberOfComments(15);
+        // 1 Market Street, San Francisco CA
+        challenge.setLocation(new ParseGeoPoint(37.793992, -122.394896));
+        challenge.setPrize("Asking for $10 for beer");
+        challenge.setCategory("Dare");
+        challenge.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                // Hack since we don't have a setter
+                ParseQuery<User> query = ParseQuery.getQuery(User.class);
+                query.whereEqualTo("objectId","vUpSb4YwWd");
+                User user = null;
+                try {
+                    user = query.find().get(0);
+                    challenge.put("backer", user);
+                    challenge.save();
+                } catch (ParseException e1) {
+                    e1.printStackTrace();
+                }
+
+                challenge.complete(new GetCallback<Challenge>() {
+                    @Override
+                    public void done(Challenge completedChallenge, ParseException e) {
+                        // This challenge should be backed!
+                        assertNotNull(completedChallenge.backer());
+                        assertEquals(completedChallenge.getStatus(), ChallengeStatus.COMPLETED);
+                        countDown();
+                    }
+                });
+            }
+        });
+        await();
+    }
+
+    public void testVerify() {
+        // Let's create a challenge first
+        final Challenge challenge = new Challenge();
+        challenge.setTitle("Backed challenge");
+        challenge.setDescription("Will record a video and post!");
+        challenge.setExpiryDate(new Date());
+        challenge.setNumberOfLikes(100);
+        challenge.setNumberOfViews(205678);
+        challenge.setNumberOfComments(15);
+        // 1 Market Street, San Francisco CA
+        challenge.setLocation(new ParseGeoPoint(37.793992, -122.394896));
+        challenge.setPrize("Asking for $10 for beer");
+        challenge.setCategory("Dare");
+        challenge.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                // Hack since we don't have a setter
+                ParseQuery<User> query = ParseQuery.getQuery(User.class);
+                query.whereEqualTo("objectId","vUpSb4YwWd");
+                User user = null;
+                try {
+                    user = query.find().get(0);
+                    challenge.put("backer", user);
+                    challenge.save();
+                } catch (ParseException e1) {
+                    e1.printStackTrace();
+                }
+
+                challenge.verify(new GetCallback<Challenge>() {
+                    @Override
+                    public void done(Challenge verifiedChallenge, ParseException e) {
+                        // This challenge should be backed!
+                        assertNotNull(verifiedChallenge.backer());
+                        assertEquals(verifiedChallenge.getStatus(), ChallengeStatus.VERIFIED);
                         countDown();
                     }
                 });
