883f7e932910031547885e020a987d69fc1af6d2
Pritesh Shah <pritesh.shah@magnet.com>
2015-03-18 01:30:37 -0700

    Removed VideoFragment

--- ChallengerApp/app/src/main/java/com/example/vikramjeet/challengerapp/fragments/MediaPagerAdapter.java
+++ ChallengerApp/app/src/main/java/com/example/vikramjeet/challengerapp/fragments/MediaPagerAdapter.java
@@ -3,11 +3,17 @@ package com.example.vikramjeet.challengerapp.fragments;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
+import com.example.vikramjeet.challengerapp.configurations.Auth;
 import com.example.vikramjeet.challengerapp.models.Challenge;
-import com.example.vikramjeet.challengerapp.models.MediaType;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 public class MediaPagerAdapter extends FragmentPagerAdapter {
+
+    private static final String TAG = MediaPagerAdapter.class.getName();
 
     private final Challenge mChallenge;
     private String tabTitles[] = {"Info", "Tagline"};
@@ -26,13 +32,48 @@ public class MediaPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         if (position == 0) {
             if (mChallenge.isCreatedMediaVideo()) {
-                return VideoFragment.newInstance(mChallenge.getCreatedMediaId(), MediaType.CREATED);
+                YouTubePlayerSupportFragment playerFragment = YouTubePlayerSupportFragment
+                        .newInstance();
+                playerFragment.initialize(Auth.KEY,
+                        new YouTubePlayer.OnInitializedListener() {
+                            @Override
+                            public void onInitializationSuccess(
+                                    YouTubePlayer.Provider provider,
+                                    YouTubePlayer youTubePlayer, boolean b) {
+                                youTubePlayer.cueVideo(mChallenge.getCreatedMediaId());
+                            }
+
+                            @Override
+                            public void onInitializationFailure(
+                                    YouTubePlayer.Provider provider,
+                                    YouTubeInitializationResult result) {
+                                Log.e(TAG, result.toString());
+                            }
+                        });
+                return playerFragment;
             } else {
                 return ImageFragment.newInstance(mChallenge.getCreatedMedia());
             }
         } else if (position == 1) {
             if (mChallenge.isCompletedMediaVideo()) {
-                return VideoFragment.newInstance(mChallenge.getCompletedMediaId(), MediaType.COMPLETED);
+                YouTubePlayerSupportFragment playerFragment = YouTubePlayerSupportFragment.newInstance();
+                playerFragment.initialize(Auth.KEY,
+                        new YouTubePlayer.OnInitializedListener() {
+                            @Override
+                            public void onInitializationSuccess(
+                                    YouTubePlayer.Provider provider,
+                                    YouTubePlayer youTubePlayer, boolean b) {
+                                youTubePlayer.cueVideo(mChallenge.getCompletedMediaId());
+                            }
+
+                            @Override
+                            public void onInitializationFailure(
+                                    YouTubePlayer.Provider provider,
+                                    YouTubeInitializationResult result) {
+                                Log.e(TAG, result.toString());
+                            }
+                        });
+                return playerFragment;
             } else {
                 return ImageFragment.newInstance(mChallenge.getCompletedMedia());
             }
