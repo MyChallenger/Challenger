a6f563af4b32b052f3c2bacd7b2a6a2fd653589e
vrumale-cpi <vinutha.rumale@chargepoint.com>
2015-03-18 00:09:13 -0700

    Profile and leader board
    Profile gives detailed view of photo, score and rank. Clicking on the rank button takes you to leader board.
    
    Signed-off-by: vrumale-cpi <vinutha.rumale@chargepoint.com>

--- ChallengerApp/app/src/main/java/com/example/vikramjeet/challengerapp/activities/ProfileActivity.java
+++ ChallengerApp/app/src/main/java/com/example/vikramjeet/challengerapp/activities/ProfileActivity.java
@@ -1,26 +1,74 @@
 package com.example.vikramjeet.challengerapp.activities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.vikramjeet.challengerapp.R;
+import com.example.vikramjeet.challengerapp.models.User;
+import com.parse.ParseUser;
+import com.squareup.picasso.Picasso;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class ProfileActivity extends ActionBarActivity {
+    @InjectView(R.id.ivBG)
+    ImageView ivBG;
+    @InjectView(R.id.ivProfileFG)
+    ImageView ivProfile;
+    @InjectView(R.id.tvName)
+    TextView tvName;
+    @InjectView(R.id.tvLocation)
+    TextView tvLocation;
+    @InjectView(R.id.tvPoints)
+    TextView tvPoint;
+    @InjectView(R.id.tvRank)
+    Button btnRank;
+    @InjectView(R.id.tvChallengesCount)
+    TextView tvChallengesCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_profile);
+        ButterKnife.inject(this);
         //Action Bar
+
+        getUserData();
+
+    }
+
+    private void getUserData() {
+       User user = (User)ParseUser.getCurrentUser();
+        if(user == null) {
+            Log.e("wtf", "wtf");
+            return;
+        }
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setTitle("Welcome Mike");
+        actionBar.setTitle("Welcome"+ user.getName());
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+        ivProfile.setEnabled(false);
+        Picasso.with(this).load(user.getPhotoURL()).into(ivBG);
+        tvName.setText(user.getName());
+
+        tvLocation.setText(user.getLocation());
+       // tvPoint.setText("100");
+        tvPoint.setText("Points Earned: " + user.getPointsEarned());
+        btnRank.setText("Your Rank: "+user.getLeaderBoardRank());
+        tvChallengesCount.setText("Challenges Accepted: " +user.getChallengesBacked()+ user.getChallengesCompleted());
+
 
     }
 
@@ -45,5 +93,11 @@ public class ProfileActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void onRankBoard(View view) {
+        Intent intent = new Intent(this, LeaderboardActivity.class);
+        startActivity(intent);
+
+
     }
 }
